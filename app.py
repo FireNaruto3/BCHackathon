9535e6189bd46397c6c88b462eea0a6f5ab6b520
Jonathan Lin <jonathanlin2020@gmail.com>
2024-06-08 15:33:26 -0700

    hello

--- app.py
+++ app.py
@@ -7,4 +7,8 @@ def home():
     return render_template("index.html")
 
 @app.route("/eyeDisease.html")
-def: 
+def test():
+    return render_template("eyeDisease.html")
+
+if(__name__ == '__main__'):
+    app.run(debug = True)
