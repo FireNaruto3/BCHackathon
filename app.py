24028574090303dd2b24c005f80a69579ba79426
Jonathan Lin <jonathanlin2020@gmail.com>
2024-06-08 15:17:53 -0700

    changed stuff

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/eyeDisease.html")
+def: 
